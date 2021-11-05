5a790396d8477efc18a0410a5df1d798803080b2
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-05 09:05:56 +0100

    transfer int array to phone number

---
+++ Open-Lab-04.04/Open-Lab-04.04/PhoneNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Open_Lab_04._04
+{
+    public class PhoneNumbers
+    {
+        public string FormatPhoneNumber(int[] numbers)
+        {
+            string number = ($"({numbers[0]}");
+            number += numbers[1];
+            number += numbers[2];
+            number += ($") {numbers[3]}");
+            number += numbers[4];
+            number += numbers[5];
+            number += ($"-{numbers[6]}");
+            number += numbers[7];
+            number += numbers[8];
+            number += numbers[9];
+            return number;
+        }
+    }
+}
